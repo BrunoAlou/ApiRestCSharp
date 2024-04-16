332751d7c8792837667486ac59c280d27c002109
Bruno Alves Lourenço <bruunieng@gmail.com>
2024-04-16 12:27:36 -0300

    Add files via upload

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using SeniorApi.Models;
+
+namespace SeniorApi.Repositories
+{
+  public static class UserRepository
+  {
+    private static readonly List<User> _users = new List<User>
+        {
+            new User
+            {
+                Id = 1,
+                UserCode = "001",
+                Name = "teste01",
+                CPF = "11111111111",
+                UF = "GO",
+                BirthDate = "01/01/2024",
+                UserName = "teste1",
+                Password = "123456",
+                Role = "manager"
+            }
+        };
+
+    public static User Get(string username, string password)
+    {
+      return _users.FirstOrDefault(x => x.UserName.ToLower() == username.ToLower() && x.Password == password);
+    }
+  }
+}
